2ac3379d583118a83cbde92f2f9e9b701beb7bc4
IMH51 <ianmartinharrison@outlook.com>
2018-12-20 11:00:30 +0000

    completed marvel heroine app code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,44 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
+    @powers = Power.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  def power_filter
+    @heroines = []
+    Heroine.all.map do |heroine|
+      if heroine.power_id == params[:power_id].to_i
+        @heroines << heroine
+      end
+    end
+    @powers = Power.all
+    render :filter
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
